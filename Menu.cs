d662e2aba1e445d866c7816188614c89a237f97b
jorisgoeree <joris_05@hotmail.com>
2023-10-02 14:26:15 +0200

    Created-Welcome-screen-and-login-menu
    - AsciiArt.cs contains the ascii art
    - DisplayAsciiArt.cs contains the logic for displaying the art
    - Menu.cs Contains the menu logic
    - Program.cs contains the Main program loop and login menu

---
+++ Menu.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * HOW TO USE MENU
+ * You need 2 parameters
+ *      string text: the text to be displayed above the menu
+ *      List<string> options: a list with the options the user can select
+ * 
+ * Call Menu.Start(text, options).
+ * When the user selects an option the method returns an integer that corresponds to the position in the list.
+ * The Menu Automatically displays the header at the top.
+ */
+public static class Menu
+{
+    static int selectedOption = 0;
+
+    static ConsoleKeyInfo keyInfo;
+
+    public static int Start(string text, List<string> options)
+    {
+        selectedOption = 0;
+        Console.CursorVisible = false;
+        bool inMenu = true;
+        while (inMenu)
+        {
+            Console.Clear();
+            DisplayAsciiArt.Header();
+            DisplayMenuLocation();
+
+            DisplayOptions(text, options);
+            keyInfo = Console.ReadKey();
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    if (selectedOption > 0)
+                        selectedOption--;
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (selectedOption < options.Count - 1)
+                        selectedOption++;
+                    break;
+                case ConsoleKey.Enter:
+                    inMenu = false;
+                    return selectedOption;
+            }
+        }
+        return -1;
+    }
+
+    private static void DisplayOptions(string text, List<string> options)
+    {
+        Console.WriteLine(text);
+
+        for (int i = 0; i < options.Count; i++)
+        {
+            if (i == selectedOption)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.Gray;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            Console.WriteLine($"  {options[i].PadRight(25)}");
+        }
+
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.WriteLine("\n\nUse the up/down arrow keys to navigate, press Enter to select");
+        Console.ResetColor();
+    }
+
+    private static void DisplayMenuLocation()
+    {
+        Console.ForegroundColor= ConsoleColor.DarkGray;
+        Console.WriteLine("Main Menu > Current Movies [FUNCTIONALITY OF MENU BAR NOT IMPLEMENTED]\n");
+        Console.ResetColor();
+    }
+}
