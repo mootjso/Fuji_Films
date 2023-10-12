dfb16469bb4d869defcbf5f1df8236b6bcaff101
jorisgoeree <joris_05@hotmail.com>
2023-10-12 15:56:13 +0200

    Changed ascii art and font colors

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ public class Program
     {
         while (true)
         {
-            AddMovieToSchedule.Start();
             // Login Menu
             string menuText = "Welcome to Ships Cinema!\n\nAre you an existing user or would you like to register a new account?\n";
             List<string> menuOptions = new() { "I am an existing user", "Register a new account", "Exit" };
@@ -12,8 +11,8 @@ public class Program
             bool loggedIn = false;
             while (!loggedIn)
             {
-                DisplayAsciiArt.Standby();
-                DisplayAsciiArt.OpeningLogo();
+                //DisplayAsciiArt.Standby();
+                //DisplayAsciiArt.OpeningLogo();
 
                 int selection = Menu.Start(menuText, menuOptions);
                 switch (selection)
