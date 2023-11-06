8f031a923db3fc808f6ea9241bb1010101ae0198
Triwan Paragh <triwanparagh@gmail.com>
2023-11-06 14:18:07 +0100

    Testing User DISCORD
    JORIS

--- DisplayMovie.cs
+++ DisplayMovie.cs
@@ -1,6 +1,7 @@
 ﻿public static class DisplayMovie
 {
     static List<Movie> movies = JSONMethods.ReadJSON<Movie>(JSONMethods.MovieFileName);
+    //JORISSSSSSSSSSSSS
     public static void Start()
     {
         string menuText = "List of Movies:";
