63187b8c1e8c595f8201faf484393a9560a08f9f
Mo <114401054+mootjso@users.noreply.github.com>
2023-11-14 21:16:15 +0100

    Update ReservationHandler.cs

--- ReservationHandler.cs
+++ ReservationHandler.cs
@@ -23,6 +23,45 @@ public static class ReservationHandler
         return ReservationIDString;
     }
 
+    public static void AddTicketsToReservations()
+    {
+        var tickets = JSONMethods.ReadJSON<Ticket>("tickets.json");
+        var reservations = JSONMethods.ReadJSON<Reservation>("reservations.json");
+
+        foreach (var ticket in tickets)
+        {
+            bool reservationExists = false;
+            foreach (var reservation in reservations)
+            {
+                if (reservation.ShowId == ticket.ShowId)
+                {
+                    if (reservation.ReservationId == ticket.ReservationId)
+                    {
+                        reservationExists = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!reservationExists)
+            {
+                // HIER MOET NOG DE SHOW GEVONDEN WORDEN ZODAT WE DE MOVIE TITLE HEBBEN
+                var show = GetShowById(ticket.ShowId);
+                if (show != null)
+                {
+                    int movieId = show.MovieId;
+                    var reservation = new Reservation(ticket.ReservationId, ticket.UserId, ticket.ShowId, ); //Hier moet de movie id
+
+                    reservations.Add(reservation);
+                }
+                
+            }
+        }
+
+        // HIER MOET DE LIST VAN RESERVATIONS DIE VONDEN ZIJN GESAVED WORDEN NAAR JSON
+        JSONMethods.WriteToJSON(reservations, "reservations.json");
+    }
+
     public static List<Reservation> GetReservationsByUser(User user)
     {
         var reservationsUser = new List<Reservation>();
@@ -31,4 +70,6 @@ public static class ReservationHandler
                 reservationsUser.Add(reservation);
         return reservationsUser;
     }
+
+    public 
 }
