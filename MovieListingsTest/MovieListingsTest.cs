f4354fdf57526821ac7be2c1997f00459c82bbe4
xSha14 <timonp22@gmail.com>
2023-11-17 17:28:27 +0100

    Added unittests
    - GetMovieById
    - GetMovieTitles

--- MovieListingsTest/MovieListingsTest.cs
+++ MovieListingsTest/MovieListingsTest.cs
@@ -37,6 +37,43 @@ namespace MovieListingsTest
             Assert.AreEqual(105, movie.Runtime);
             Assert.AreEqual(18, movie.AgeRating);
         }
-    }
 
+        [TestMethod]
+        public void TestGetMovieById()
+        {
+            List<Movie> movieToWrite = new()
+        {
+            new Movie(1, "Movie1", "en", "First Movie", new List<string> { "Adventure", "Horror" }, 105, 18),
+            new Movie (2, "Movie2", "nl", "Second Movie", new List<string> { "Thriller", "Comedy" }, 120, 16)
+        };
+            JSONMethods.WriteToJSON(movieToWrite, _fileName);
+
+            Movie? movie = MovieHandler.GetMovieById(2);
+            Movie? movieNull = MovieHandler.GetMovieById(3);
+            
+            Assert.IsNull(movieNull);
+            Assert.IsNotNull(movie);
+            Assert.AreEqual(2, movie.Id);
+            Assert.AreEqual("Movie2", movie.Title);
+            Assert.AreEqual("nl", movie.Language);
+            Assert.AreEqual("Second Movie", movie.Description);
+            CollectionAssert.AreEqual(new List<string> { "Thriller", "Comedy" }, movie.Genres);
+            Assert.AreEqual(120, movie.Runtime);
+            Assert.AreEqual(16, movie.AgeRating);
+        }
+
+        [TestMethod]
+        public void TestGetMovieTitles()
+        {
+            List<Movie> movieToWrite = new()
+            {
+                new Movie(1, "Movie1", "en", "First Movie", new List<string> { "Adventure", "Horror" }, 105, 18),
+                new Movie (2, "Movie2", "nl", "Second Movie", new List<string> { "Thriller", "Comedy" }, 120, 16)
+            };
+            JSONMethods.WriteToJSON(movieToWrite, _fileName);
+
+            List<string> movieTitles = MovieHandler.GetMovieTitles();
+            CollectionAssert.AreEqual(new List<string> { "Movie1", "Movie2" }, movieTitles);
+        }
+    }
 }
