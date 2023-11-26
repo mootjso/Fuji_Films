8e5c27acc28a961ebb3111874bda3411730d21af
wen <wen.bei@outlook.com>
2023-11-26 20:07:23 +0100

    added unittests reservationhandler

--- TicketPurchaseTest/TicketPurchaseTest.cs
+++ TicketPurchaseTest/TicketPurchaseTest.cs
@@ -80,4 +80,58 @@ public class TicketHandlerTests
         Assert.IsTrue(actualTotalPrice > 0);
     }
 }
+
+[TestClass]
+public class ReservationHandlerTests
+{
+    protected static string FileName2 = ReservationHandler.FileName;
+
+
+
+    [ClassInitialize]
+    public static void ClassInitialize(TestContext context)
+    {
+        // Clear the  JSON file
+        File.WriteAllText(FileName2, "[]");
+
+
+        List<Reservation> testReservations = new List<Reservation>
+        {
+            new Reservation("c5e9-9f7e-1985", 1, 1, 2, 8, 5),
+            new Reservation("abc1-def2-ghi3", 2, 3, 4, 10, 7),
+            new Reservation("xyz9-uvw8-pqr7", 3, 5, 6, 15, 3),
+
+        };
+        JSONMethods.WriteToJSON(testReservations, FileName2);
+
+    }
+    
+    [TestMethod]
+    public void TestReservationID()
+    {
+        string reservationId = ReservationHandler.GetReservationID();
+
+        Assert.IsNotNull(reservationId);
+        Assert.AreEqual(14, reservationId.Length); // Check if the generated ID has the correct length
+    }
+
+    [TestMethod]
+    public void TestWriteReservationsToJSON()
+    {
+        List<Reservation> reservations = JSONMethods.ReadJSON<Reservation>(FileName2).ToList();
+        Reservation reservationTest = reservations[2];
+
+        Assert.AreEqual(3, reservations.Count); // Check if the number of reservations is correct
+        
+        Assert.AreEqual("xyz9-uvw8-pqr7", reservationTest.ReservationId);
+        Assert.AreEqual(3, reservationTest.UserId);
+        Assert.AreEqual(5, reservationTest.ShowId);
+        Assert.AreEqual(6, reservationTest.MovieId);
+        Assert.AreEqual(15, reservationTest.Row);
+        Assert.AreNotEqual(4, reservationTest.Column);
+
+    }
+
+
+}
 }
