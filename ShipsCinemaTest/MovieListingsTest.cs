f0f06e9b629b37db38b5ed6ce7c02e1339f7bd58
xSha14 <timonp22@gmail.com>
2023-11-27 16:24:43 +0100

    fixed unittest and renamed

--- ShipsCinemaTest/MovieListingsTest.cs
+++ ShipsCinemaTest/MovieListingsTest.cs
@@ -23,9 +23,9 @@ namespace MovieListingsTest
         public void TestReadMoviesFromJSON()
         {
             List<Movie> movieToWrite = new()
-            {
+        {
             new Movie(1, "Movie1", "en", "First Movie", new List<string> { "Adventure", "Horror" }, 105, 18)
-            };
+        };
             JSONMethods.WriteToJSON(movieToWrite, _fileName);
             List<Movie> readMovie = JSONMethods.ReadJSON<Movie>(_fileName).ToList();
             Movie movie = readMovie[0];
@@ -42,10 +42,10 @@ namespace MovieListingsTest
         public void TestGetMovieById()
         {
             List<Movie> movieToWrite = new()
-            {
+        {
             new Movie(1, "Movie1", "en", "First Movie", new List<string> { "Adventure", "Horror" }, 105, 18),
             new Movie (2, "Movie2", "nl", "Second Movie", new List<string> { "Thriller", "Comedy" }, 120, 16)
-            };
+        };
             JSONMethods.WriteToJSON(movieToWrite, _fileName);
 
             Movie? movie = MovieHandler.GetMovieById(2);
@@ -65,15 +65,18 @@ namespace MovieListingsTest
         [TestMethod]
         public void TestGetMovieTitles()
         {
+            Movie movie1 = new(1, "Movie1", "en", "First Movie", new List<string> { "Adventure" }, 105, 18);
+            string movieString1 = $"{"Movie1",-30} | {"English",-10} | {"Adventure" ,-30} | {"105" + " min",-8} | 18+";
+
             List<Movie> movieToWrite = new()
             {
-                new Movie(1, "Movie1", "en", "First Movie", new List<string> { "Adventure", "Horror" }, 105, 18),
-                new Movie (2, "Movie2", "nl", "Second Movie", new List<string> { "Thriller", "Comedy" }, 120, 16)
+                movie1,
+                new Movie(2, "Movie 2", "en", "kaas", new List<string> { "Family" }, 120, 12)
             };
             JSONMethods.WriteToJSON(movieToWrite, _fileName);
 
             List<string> movieTitles = MovieHandler.GetMovieTitles();
-            CollectionAssert.AreEqual(new List<string> { "Movie1", "Movie2" }, movieTitles);
+            Assert.AreEqual(movieString1, movieTitles[0]);
         }
     }
 }
