d6ffd2d1f2a1fa16e9b9582fc4b69ff36c9d4a29
Mo <mohamedsouabi143@gmail.com>
2023-11-27 15:00:02 +0100

    Create Revenue.cs

---
+++ ShipsCinema/Revenue.cs
@@ -0,0 +1,12 @@
+﻿public class Revenue
+{
+    public int MovieId { get; set; }
+    public string MovieTitle { get; set; }
+    public double TotalRevenue;
+    public Revenue(int movieId, string movieTitle, double totalRevenue)
+    {
+        MovieId = movieId;
+        MovieTitle = movieTitle;
+        TotalRevenue = totalRevenue;
+    }
+}
