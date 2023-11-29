7c9b41cac4d64f8316492c50b4f6056e5878fb81
Mo <mohamedsouabi143@gmail.com>
2023-11-29 10:18:31 +0100

    Update Revenue.cs
    Removed the movieid because this is already inside the show which u can find with the showid so the movieid is not needed.

--- ShipsCinema/Revenue.cs
+++ ShipsCinema/Revenue.cs
@@ -1,13 +1,11 @@
 ﻿public class Revenue
 {
     public int ShowId { get; set; }
-    public int MovieId { get; set; }
     public string MovieTitle { get; set; }
     public double TotalRevenue;
-    public Revenue(int showId, int movieId, string movieTitle, double totalRevenue)
+    public Revenue(int showId, string movieTitle, double totalRevenue)
     {
         ShowId = showId;
-        MovieId = movieId;
         MovieTitle = movieTitle;
         TotalRevenue = totalRevenue;
     }
