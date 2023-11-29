662791ce024e508c82634779937d6d150e8b1207
Mo <mohamedsouabi143@gmail.com>
2023-11-29 11:16:52 +0100

    Update CheckOutHandler.cs
    Making the check-out methode.
    user can only pay with card.

--- ShipsCinema/CheckOutHandler.cs
+++ ShipsCinema/CheckOutHandler.cs
@@ -54,8 +54,34 @@ public class CheckOutHandler
         return revenue;
     }
 
-    public static void CheckOut()
+    public static bool CheckOut()
     {
+        AdHandler.DisplaySnacks();
+        Console.WriteLine("Please enter your credit card number:\nEXAMPLE 4321-2432-2432\n");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        string creditCardInput = Console.ReadLine();
+        if (creditCardInput.Contains("-"));
+            string creditCard = $"{creditCardInput.Substring(0,4)}{creditCardInput.Substring(5,4)}{creditCardInput.Substring(10,4)}";
 
+        if (creditCard.Length != 12)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Credit card does NOT exist!\nTRANSACTION CANCELLED\n");
+            Console.WriteLine("Press any button to continue");
+            Console.ReadLine();
+            Console.Clear();
+            return false;
+        }
+
+
+        Console.WriteLine("Please input the experation date:\nEXAMPLE MM/YY, 02/25\n");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        string experationCodeInput = Console.ReadLine();
+        if (experationCodeInput.Contains("/"));
+            string experationCode = $"{experationCodeInput.Substring(0,2)}{experationCodeInput.Substring(3,2)}";
+
+        Console.WriteLine("Please input the CVC code (on the back):\nEXAMPLE 454\n");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        string CVC = Console.ReadLine();
     }
 }
