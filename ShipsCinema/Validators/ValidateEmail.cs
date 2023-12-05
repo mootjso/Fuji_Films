20fd1ddfbf8fed974f0810dfb88b887eaa91fe4f
DrBoomBoom69 <triwanparagh@gmail.com>
2023-12-05 23:40:24 +0100

    Splitted Validators
    -

---
+++ ShipsCinema/Validators/ValidateEmail.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+public static class ValidateEmail
+{
+    public static bool IsValid(string email)
+    {
+        if (email == null)
+            return false;
+
+        string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+        Regex regex = new Regex(pattern);
+
+        return regex.IsMatch(email);
+    }
+}
