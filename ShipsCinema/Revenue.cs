d4b24d1623664dc57950b2d67a84635adb5b86cb
Mo <mohamedsouabi143@gmail.com>
2023-12-05 18:34:49 +0100

    Updated to include the month in the json

--- ShipsCinema/Revenue.cs
+++ ShipsCinema/Revenue.cs
@@ -3,10 +3,12 @@
     public int ShowId { get; set; }
     public string MovieTitle { get; set; }
     public double TotalRevenue;
-    public Revenue(int showId, string movieTitle, double totalRevenue)
+    public int MonthDate;
+    public Revenue(int showId, string movieTitle, double totalRevenue, int monthDate)
     {
         ShowId = showId;
         MovieTitle = movieTitle;
         TotalRevenue = totalRevenue;
+        MonthDate = monthDate;
     }
 }
