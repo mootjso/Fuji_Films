f8bd51280914ff927e6a4e0a9a29168fd9b8a6d6
DrBoomBoom69 <triwanparagh@gmail.com>
2023-12-06 00:29:53 +0100

    Updated LoginHandlerTest
    Changed the Paths/References because of some files being put into differnt files.

--- ShipsCinemaTest/Unit Tests/LogInHandlerTest.cs
+++ ShipsCinemaTest/Unit Tests/LogInHandlerTest.cs
@@ -1,134 +1,133 @@
-namespace LogInHandlerTest;
-
-[TestClass]
-public class TestLoginHandler
+namespace LoginHandlerTest
 {
-    private static string _fileName = LoginHandler.FileName;
-    private static List<User> _originalFileName = JSONMethods.ReadJSON<User>(_fileName).ToList();
-
-    [ClassInitialize]
-    public static void ClassInitialize(TestContext context)
+    [TestClass]
+    public class TestLoginHandler
     {
-        // Clear the test JSON file
-        File.WriteAllText(_fileName, "[]");
+        private static string _fileName = LoginHandler.FileName;
+        private static List<User> _originalFileName = JSONMethods.ReadJSON<User>(_fileName).ToList();
 
-        List<User> testUsers = new List<User>
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
         {
-            new User(1, "John", "Doe", "john.doe@outlook.com", "password123", "0612345678"),
-            new User(2, "Barld", "Boot", "barld.boot@gmail.com", "securepass", "0612345679", true)
-        };
-        JSONMethods.WriteToJSON(testUsers, _fileName);
+            // Clear the test JSON file
+            File.WriteAllText(_fileName, "[]");
 
-    }
+            List<User> testUsers = new List<User>
+            {
+                new User(1, "John", "Doe", "john.doe@outlook.com", "password123", "0612345678"),
+                new User(2, "Barld", "Boot", "barld.boot@gmail.com", "securepass", "0612345679", true)
+            };
+            JSONMethods.WriteToJSON(testUsers, _fileName);
+        }
 
-    [ClassCleanup]
-    public static void CleanupJSONFile()
-    {
-       JSONMethods.WriteToJSON(_originalFileName, _fileName); 
-    }
+        [ClassCleanup]
+        public static void CleanupJSONFile()
+        {
+            JSONMethods.WriteToJSON(_originalFileName, _fileName);
+        }
 
-    [TestMethod]
-    public void TestReadFromJSON()
-    {
-        User user = LoginHandler.Users[0];
-        User user_admin = LoginHandler.Users[1];
+        [TestMethod]
+        public void TestReadFromJSON()
+        {
+            User user = LoginHandler.Users[0];
+            User user_admin = LoginHandler.Users[1];
 
-        Assert.AreEqual(1, user.Id);
-        Assert.AreEqual("John", user.FirstName);
-        Assert.AreEqual("Doe", user.LastName);
-        Assert.AreEqual("john.doe@outlook.com", user.Email);
-        Assert.AreEqual("password123", user.Password);
-        Assert.AreEqual("0612345678", user.PhoneNumber);
+            Assert.AreEqual(1, user.Id);
+            Assert.AreEqual("John", user.FirstName);
+            Assert.AreEqual("Doe", user.LastName);
+            Assert.AreEqual("john.doe@outlook.com", user.Email);
+            Assert.AreEqual("password123", user.Password);
+            Assert.AreEqual("0612345678", user.PhoneNumber);
 
-        Assert.IsFalse(user.IsAdmin);
+            Assert.IsFalse(user.IsAdmin);
 
-        Assert.IsTrue(user_admin.IsAdmin);
-    }
+            Assert.IsTrue(user_admin.IsAdmin);
+        }
 
-    [TestMethod]
-    public void TestValidateNameInput()
-    {
-        bool result = LoginHandler.ValidateNameInput("John");
+        [TestMethod]
+        public void TestValidateNameInput()
+        {
+            bool result = ValidateNameInput.IsValid("John");
 
-        bool wrongResult = LoginHandler.ValidateNameInput("Po");
-        bool nullResult = LoginHandler.ValidateNameInput(null);
-        bool resultNumber = LoginHandler.ValidateNameInput("John5");
+            bool wrongResult = ValidateNameInput.IsValid("Po");
+            bool nullResult = ValidateNameInput.IsValid(null);
+            bool resultNumber = ValidateNameInput.IsValid("John5");
 
-        Assert.IsTrue(result);
+            Assert.IsTrue(result);
 
-        Assert.IsFalse(wrongResult);
-        Assert.IsFalse(nullResult);
-        Assert.IsFalse(resultNumber);
-    }
+            Assert.IsFalse(wrongResult);
+            Assert.IsFalse(nullResult);
+            Assert.IsFalse(resultNumber);
+        }
 
-    [TestMethod]
-    public void TestValidatePassword()
-    {
-        bool lowercaseMissing = LoginHandler.ValidatePassword("PASSWORD123");
-        bool digitMissing = LoginHandler.ValidatePassword("PASSWORDS");
-        bool UppercaseMissing = LoginHandler.ValidatePassword("password123");
-        bool resultNull = LoginHandler.ValidatePassword(null);
+        [TestMethod]
+        public void TestValidatePassword()
+        {
+            bool lowercaseMissing = ValidatePassword.IsValid("PASSWORD123");
+            bool digitMissing = ValidatePassword.IsValid("PASSWORDS");
+            bool UppercaseMissing = ValidatePassword.IsValid("password123");
+            bool resultNull = ValidatePassword.IsValid(null);
 
+            bool resultLong = ValidatePassword.IsValid("PASSwooooooooooooooooooooooooooooooooooord123");
+            bool resultNormal = ValidatePassword.IsValid("RightPassWord123");
 
-        bool resultLong = LoginHandler.ValidatePassword("PASSwooooooooooooooooooooooooooooooooooord123");
-        bool resultNormal = LoginHandler.ValidatePassword("RightPassWord123");
+            Assert.IsFalse(lowercaseMissing);
+            Assert.IsFalse(digitMissing);
+            Assert.IsFalse(UppercaseMissing);
+            Assert.IsFalse(resultNull);
 
-        Assert.IsFalse(lowercaseMissing);
-        Assert.IsFalse(digitMissing);
-        Assert.IsFalse(UppercaseMissing);
-        Assert.IsNotNull(resultNull);
+            Assert.IsTrue(resultLong);
+            Assert.IsTrue(resultNormal);
+        }
 
-        Assert.IsTrue(resultLong);
-        Assert.IsTrue(resultNormal);
-    }
+        [TestMethod]
+        public void TestValidatePhoneNumber()
+        {
+            bool resultShort = ValidatePhoneNumber.IsValid("31063");
+            bool resultLong = ValidatePhoneNumber.IsValid("31063432693733101");
+            bool resultLetters = ValidatePhoneNumber.IsValid("31063ds21");
+            bool resultNegative = ValidatePhoneNumber.IsValid("-067128249");
+            bool resultSymbol = ValidatePhoneNumber.IsValid("+067128249");
+            bool resultNull = ValidatePhoneNumber.IsValid(null);
 
-    [TestMethod]
-    public void TestValidatePhoneNumber()
-    {
-        bool resultShort = LoginHandler.ValidatePhoneNumber("31063");
-        bool resultLong = LoginHandler.ValidatePhoneNumber("31063432693733101");
-        bool resultLetters = LoginHandler.ValidatePhoneNumber("31063ds21");
-        bool resultNegative = LoginHandler.ValidatePhoneNumber("-067128249");
-        bool resultSymbol = LoginHandler.ValidatePhoneNumber("+067128249");
-        bool resultNull = LoginHandler.ValidatePhoneNumber(null);
+            bool resultGood = ValidatePhoneNumber.IsValid("067128249");
+            bool resultGood2 = ValidatePhoneNumber.IsValid("067128249142649");
 
-        bool resultGood = LoginHandler.ValidatePhoneNumber("067128249");
-        bool resultGood2 = LoginHandler.ValidatePhoneNumber("067128249142649");
+            Assert.IsFalse(resultLong);
+            Assert.IsFalse(resultShort);
+            Assert.IsFalse(resultLetters);
+            Assert.IsFalse(resultNegative);
+            Assert.IsFalse(resultSymbol);
+            Assert.IsFalse(resultNull);
 
-        Assert.IsFalse(resultLong);
-        Assert.IsFalse(resultShort);
-        Assert.IsFalse(resultLetters);       
-        Assert.IsFalse(resultNegative); 
-        Assert.IsFalse(resultSymbol);
-        Assert.IsFalse(resultNull);
+            Assert.IsTrue(resultGood);
+            Assert.IsTrue(resultGood2);
+        }
 
-        Assert.IsTrue(resultGood);
-        Assert.IsTrue(resultGood2);
-    }
+        [TestMethod]
+        public void TestValidateEmail()
+        {
+            bool missingAt = ValidateEmail.IsValid("Johnoutlook.com");
+            bool resultNull = ValidateEmail.IsValid(null);
+            bool missingDomain = ValidateEmail.IsValid("John@.com");
+            bool missingDot = ValidateEmail.IsValid("John@outlookcom");
+            bool missingTopDomain = ValidateEmail.IsValid("John@outlook.");
+            bool missingUsername = ValidateEmail.IsValid("@outlook.com");
 
-    [TestMethod]
-    public void TestValidateEmail()
-    {
-        bool missingAt = LoginHandler.ValidateEmail("Johnoutlook.com");
-        bool resultNull = LoginHandler.ValidateEmail(null);
-        bool missingDomain = LoginHandler.ValidateEmail("John@.com");
-        bool missingDot = LoginHandler.ValidateEmail("John@outlookcom");
-        bool missingTopDomain = LoginHandler.ValidateEmail("John@outlook.");
-        bool missingUsername = LoginHandler.ValidateEmail("@outlook.com");
+            bool onlyNumbers = ValidateEmail.IsValid("3983748@outlook.com");
+            bool resultAllowed = ValidateEmail.IsValid("John@outlook.com");
+            bool resultAllowed2 = ValidateEmail.IsValid("johndoe@gmail.com");
 
-        bool onlyNumbers = LoginHandler.ValidateEmail("3983748@outlook.com");
-        bool resultAllowed = LoginHandler.ValidateEmail("John@outlook.com");
-        bool resultAllowed2 = LoginHandler.ValidateEmail("johndoe@gmail.com");
+            Assert.IsFalse(missingAt);
+            Assert.IsFalse(resultNull);
+            Assert.IsFalse(missingDomain);
+            Assert.IsFalse(missingDot);
+            Assert.IsFalse(missingTopDomain);
+            Assert.IsFalse(missingUsername);
 
-        Assert.IsFalse(missingAt);
-        Assert.IsFalse(resultNull);
-        Assert.IsFalse(missingDomain);
-        Assert.IsFalse(missingDot);
-        Assert.IsFalse(missingTopDomain);
-        Assert.IsFalse(missingUsername);
-
-        Assert.IsTrue(onlyNumbers);
-        Assert.IsTrue(resultAllowed);
-        Assert.IsTrue(resultAllowed2);
+            Assert.IsTrue(onlyNumbers);
+            Assert.IsTrue(resultAllowed);
+            Assert.IsTrue(resultAllowed2);
+        }
     }
 }
