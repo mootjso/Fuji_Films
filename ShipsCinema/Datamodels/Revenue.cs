4609f172fdb69c63c6bf0cd125fbc0fbb76aa8ec
Mo <114401054+mootjso@users.noreply.github.com>
2023-12-19 12:50:11 +0100

    Update Revenue.cs

--- ShipsCinema/Datamodels/Revenue.cs
+++ ShipsCinema/Datamodels/Revenue.cs
@@ -1,14 +1,32 @@
-﻿public class Revenue
+public class Revenue
 {
     public int ShowId { get; set; }
     public string MovieTitle { get; set; }
     public double TotalRevenue;
     public int MonthDate;
-    public Revenue(int showId, string movieTitle, double totalRevenue, int monthDate)
+    public int YearDate;
+    public Revenue(int showId, string movieTitle, double totalRevenue, int monthDate, int yearDate)
     {
         ShowId = showId;
         MovieTitle = movieTitle;
         TotalRevenue = totalRevenue;
         MonthDate = monthDate;
+        YearDate = yearDate;
+    }
+}
+public class RevenueQuartly
+{
+    public int MovieId { get; set; }
+    public string MovieTitle { get; set; }
+    public double TotalRevenue;
+    public int QuarterYear;
+    public int YearDate;
+    public RevenueQuartly(int movieId, string movieTitle, double totalRevenue, int quarterYear, int yearDate)
+    {
+        MovieId = movieId;
+        MovieTitle = movieTitle;
+        TotalRevenue = totalRevenue;
+        QuarterYear = quarterYear;
+        YearDate = yearDate;
     }
 }
