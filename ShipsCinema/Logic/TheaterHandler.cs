e9eaa7438e7a2d9047473e2f0a740209aace2459
jorisgoeree <joris_05@hotmail.com>
2023-12-22 18:16:06 +0100

    Removed unnecesary line

--- ShipsCinema/Logic/TheaterHandler.cs
+++ ShipsCinema/Logic/TheaterHandler.cs
@@ -104,7 +104,6 @@ public static class TheaterHandler
 
                 // User selects a seat
                 case ConsoleKey.Enter:
-                    //Seat selectedSeat = GetSeatByRowAndColumn(theater, selectedRow, selectedColumn)!;
 
                     // Deselect seat
                     if (selectedSeats.Contains(selectedSeat) || user.IsAdmin && !selectedSeat.IsReserved && !selectedSeat.IsAvailable)
